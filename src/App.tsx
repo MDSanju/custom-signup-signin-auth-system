e1425dd2f769fab8dac90c3414944ea38aa181c2
MD Sanju <mdsanju387@gmail.com>
2023-02-05 07:08:46 +0600

    router setup

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,21 @@
 import React from "react";
+import { Route, Routes } from "react-router-dom";
+import Details from "./components/pages/Details/Details";
+import Home from "./components/pages/Home/Home";
+import Login from "./components/pages/Login/Login";
+import NoMatch from "./components/shared/NoMatch/NoMatch";
 import "./App.css";
 
 function App() {
   return (
     <div className="App">
-      <h2>Auth System!</h2>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/home" element={<Home />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/details" element={<Details />} />
+        <Route path="*" element={<NoMatch />} />
+      </Routes>
     </div>
   );
 }
